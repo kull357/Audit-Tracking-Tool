b02de1141c568192331914d9afc7dd2c1cc5469f
kull357 <143084495+kull357@users.noreply.github.com>
2025-07-28 19:06:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -535,6 +535,6 @@ def download_report():
         return jsonify({"message": "Excel file not found."}), 404
 
 if __name__ == '__main__':
-        port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
     app.run(debug=True)
